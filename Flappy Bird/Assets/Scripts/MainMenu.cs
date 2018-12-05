a67f6cdbafe36060cf3ac8dfb73b744d797a9513
Swagat <lordzed400@gmail.com>
2018-12-05 14:42:01 +0545

    Add Main Menu

---
+++ Flappy Bird/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void ChangeScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void RateGame(){
+
+	}
+
+	public void Leaderboard(){
+
+	}
+
+}
---
+++ Flappy Bird/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+
+    private Renderer quadRenderer;
+    [SerializeField] private float scrollSpeed;
+
+    void Start()
+    {
+        quadRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2( Time.time * scrollSpeed, 0);
+        quadRenderer.material.mainTextureOffset = textureOffset;
+    }
+	
+}
