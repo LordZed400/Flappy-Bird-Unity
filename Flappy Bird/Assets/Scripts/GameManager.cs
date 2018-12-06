855f2b7bfad7d990dc5993b43c3e9a0e32b634d9
Swagat <lordzed400@gmail.com>
2018-12-06 16:31:31 +0545

    Setup Basic Player Controller

--- Flappy Bird/Assets/Scripts/GameManager.cs
+++ Flappy Bird/Assets/Scripts/GameManager.cs
@@ -11,23 +11,30 @@ public class GameManager : MonoBehaviour {
 	[SerializeField] private Animator getReadyAnim, flappyAnim;
 
 	private GameObject flappy;
+	private bool start;
 
 	void Start () {
 		flappy = Instantiate(playerPrefabs[Random.Range(0,3)], playerPos.position, transform.rotation);
+		flappy.transform.parent = playerPos;
 		flappyAnim = flappy.GetComponent<Animator>();
 		background.sprite = backgroundImage[Random.Range(0,2)];
 	}
 	
 	void Update () {
-		
+		if(!start){
+			if(Input.GetMouseButtonDown(0)){
+				start = true;
+				GetReady();
+			}
+		}
 	}
 
 	public void GetReady(){
 		getReadyAnim.SetTrigger("Start");
-		flappy.GetComponentInChildren<Rigidbody2D>().gravityScale = 0.5f;
+		flappy.GetComponentInChildren<Rigidbody2D>().velocity = Vector2.zero;
+		flappy.GetComponentInChildren<Rigidbody2D>().gravityScale = 1f;
 		// flappyAnim.SetLayerWeight(1, 0);
-		flappyAnim.SetTrigger("Start");
-
+		// flappyAnim.SetTrigger("Start");
 	}
 
 }
--- Flappy Bird/Assets/Scripts/PlayerController.cs
+++ Flappy Bird/Assets/Scripts/PlayerController.cs
@@ -1,32 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-	[SerializeField] private float thrust;
+	[SerializeField] private float thrust, tiltSmooth, hoverDistance, hoverSpeed;
+	[SerializeField] private Text gameScoreText;
+	private int gameScore;
 	private bool start;
+	private float timer, y;
 	private Rigidbody2D playerRigid;
+	private Quaternion downRotation, upRotation;
+
+	
 
 	// Use this for initialization
 	void Start () {
-		thrust = 80f;
 		playerRigid = GetComponent<Rigidbody2D>();
+		downRotation = Quaternion.Euler(0, 0, -90);
+		upRotation = Quaternion.Euler(0, 0, 35);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!start){
+			timer += Time.deltaTime;
+			y = hoverDistance * Mathf.Sin(hoverSpeed * timer);
+			transform.localPosition = new Vector3 (0, y, 0);
+		}else{
+			transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+		}		
 	}
 
 	void LateUpdate(){
 		if(Input.GetMouseButtonDown(0)) {
 			if(start){
-				Debug.Log("Hello");
-				playerRigid.velocity = new Vector2(playerRigid.velocity.x, 0);
-				playerRigid.AddForce(transform.up * thrust);
+				// Debug.Log("Hello");
+				transform.rotation = upRotation;
+				playerRigid.velocity = Vector2.zero;
+				playerRigid.AddForce(Vector2.up * thrust);
+			}else{
+				start = true;
 			}
-			start = true;
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		// Update Sore
+		Destroy(col.gameObject);
+		gameScore++;
+		gameScoreText.text = gameScore + "";
+	}
+
+	void OnCollision2D(Collision2D col){
+		// Kill Flappy
+		playerRigid.simulated = false;
+		
 	}
 }
